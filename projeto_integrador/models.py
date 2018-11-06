8d0688a459a0e99c6bc5dae787a21cd2f4ac0bf4
Vinicius Oliveira <vinifladf@gmail.com>
2018-11-06 15:31:08 -0200

    Modificando dados de modelo

--- projeto_integrador/models.py
+++ projeto_integrador/models.py
@@ -5,8 +5,9 @@ from django.db import models
 # Create your models here.
 
 class Medicamento(models.Model):
-    nomeMedicamento = models.CharField(max_length=20)
-    horarios = models.TimeField()
-    nomeMedicado = models.CharField(max_length=20)
-    dosagemMedicamento = models.CharField(max_length=10)
-    intervaloHorario = models.TimeField()
+    nome = models.CharField(max_length=20)
+    intervalo = models.TimeField()
+    horario_Ini = models.TimeField()
+    qtd = models.PositiveIntegerField(blank=False, null=False)
+    limite = models.PositiveIntegerField(blank=False, null=False)
+    status = models.BooleanField(default=True)
--- projeto_integrador/serializers.py
+++ projeto_integrador/serializers.py
@@ -4,4 +4,4 @@ from projeto_integrador.models import *
 class MedicamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medicamento
-        fields = ('id', 'nomeMedicamento', 'horarios', 'nomeMedicado', 'dosagemMedicamento', 'intervaloHorario')
+        fields = ('id', 'nome', 'intervalo', 'horario_Ini', 'qtd', 'limite', 'status')
