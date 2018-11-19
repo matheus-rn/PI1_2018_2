ad36398a8a97084f8c84d84f50abcb64f10eb410
Vinícius Cantuária <cantuariavc@gmail.com>
2018-11-19 20:43:24 -0200

    Fix relation between medicine and slot

--- projeto_integrador/models.py
+++ projeto_integrador/models.py
@@ -1,6 +1,14 @@
 from __future__ import unicode_literals
 from django.db import models
 
+
+class Slot(models.Model):
+    number = models.PositiveSmallIntegerField(unique=True)
+
+    def __str__(self):
+        return self.number
+
+
 class Medicamento(models.Model):
     nome = models.CharField(max_length=20)
     intervalo = models.TimeField()
@@ -8,10 +16,4 @@ class Medicamento(models.Model):
     qtd = models.PositiveIntegerField(blank=False, null=False)
     limite = models.TimeField()
     status = models.BooleanField(default=True)
-
-class Slot(models.Model):
-    medicine = models.ForeignKey('Medicamento', on_delete=models.CASCADE)
-    number = models.PositiveSmallIntegerField(unique=True)
-
-    def __str__(self):
-        return self.number
+    slot = models.ForeignKey(Slot, on_delete=models.CASCADE)
