1207318e0d0f70e6b0385982384e7b3bffba0540
Vinícius Cantuária <cantuariavc@gmail.com>
2018-11-29 18:22:33 -0200

    Fix imports

--- projeto_integrador/tests.py
+++ projeto_integrador/tests.py
@@ -1,45 +1,56 @@
 from django.test import TestCase
-from models import Slot,Medicine
+from projeto_integrador.models import Slot, Medicine
 from datetime import date
-
-import mock
+from unittest import mock
 
 class slotTestCase(TestCase):
     def setUp(self):
         Slot.objects.create(number = 2)
 
+
     def slotExist(self):
         slot = Slot.objects.get(number = 2)
         self.assertTrue(slot)
 
+
 def fakeSlot():
     return 2
+
+
 def fakeTimeSeconds():
-    return datetime.seconds(15)
+    return date.seconds(15)
+
+
 def fakeTimeHour():
-    return datetime.time(15000)
+    return date.time(15000)
+
+
 class medicineTestCase(TestCase):
+    @mock.patch("date.seconds", fakeTimeSeconds)
+    @mock.patch("date.time", fakeTimeHour)
+    @mock.patch("Slot", fakeSlot)
 
-        @mock.patch("datetime.date.seconds", fakeTimeSeconds)
-        @mock.patch("datetime.date.time", fakeTimeHour)
-        @mock.patch("Slot", fakeSlot)
-        def setUp(self):
-            Medicine.objects.create(name = "abacate",
-                                    interval = datetime.date.seconds(),
-                                    initial_time = datetime.date.time(),
-                                    amount = 8,
-                                    limit = datetime.date.seconds(),
-                                    status = True,
-                                    slot = Slot()
-                                    )
 
-        def checkExistMedicine(self):
-                med = Medicine.objects.get(name = "abacate",
-                                        interval = 15,
-                                        initial_time = 15000 ,
-                                        amount = 8,
-                                        limit = 15,
-                                        status = True,
-                                        slot = 2
-                                        )
-                self.assertTrue(med)
+    def setUp(self):
+        Medicine.objects.create(
+            name = "abacate",
+            interval = date.seconds(),
+            initial_time = date.time(),
+            amount = 8,
+            limit = date.seconds(),
+            status = True,
+            slot = Slot()
+        )
+
+
+    def checkExistMedicine(self):
+            med = Medicine.objects.get(
+                name = "abacate",
+                interval = 15,
+                initial_time = 15000 ,
+                amount = 8,
+                limit = 15,
+                status = True,
+                slot = 2
+            )
+            self.assertTrue(med)
