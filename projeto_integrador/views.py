fc1accf7f7d9d3e16dc828b0cae9675582f24613
matheus-rn <matheus.1nascimento58@gmail.com>
2018-12-02 13:49:19 -0200

    refactoring template

--- projeto_integrador/views.py
+++ projeto_integrador/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 
 from projeto_integrador.resources import *
 from projeto_integrador.models import *
 from projeto_integrador.forms import *
+from django.contrib import messages
 
 
 def home(request):
@@ -43,15 +44,11 @@ def update_medicine(request, id):
 
 
 def delete_medicine(request, id):
-    medicine = Medicine.objects.get(id=id)
-
-    if request.method == 'POST':
-        medicine.delete()
-        return redirect('list_medicines')
-
-    return render(request, 'medicine-delete-confirm.html', {'medicine': medicine})
-
-
+    medicine = get_object_or_404(Medicine, id=id)
+    medicine.delete()
+    messages.success(request, "Medicamento "+str(medicine.name)+" deletado com sucesso")
+    return redirect('list_medicines')
+    
 # Slot
 def list_slots(request):
     slots = Slot.objects.all().order_by('number')
@@ -80,10 +77,7 @@ def update_slot(request, id):
 
 
 def delete_slot(request, id):
-    slot = Slot.objects.get(id=id)
-
-    if request.method == 'POST':
-        slot.delete()
-        return redirect('list_slots')
-
-    return render(request, 'slot-delete-confirm.html', {'slot': slot})
+    slot = get_object_or_404(Slot, id=id)
+    slot.delete()
+    messages.success(request, "Slot "+str(slot.number)+" deletado com sucesso")
+    return redirect('list_slots')
