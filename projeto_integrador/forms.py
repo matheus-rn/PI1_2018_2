36c05e396a79519957399aafa691a19af3545929
matheus-rn <matheus.1nascimento58@gmail.com>
2018-12-02 20:12:15 -0200

    Refactoring form

--- projeto_integrador/forms.py
+++ projeto_integrador/forms.py
@@ -7,15 +7,13 @@ class MedicineForm(forms.ModelForm):
 	name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Digite o nome do medicamento'}))
 	interval = forms.TimeField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Digite o intervalo'}))
 	initial_time = forms.TimeField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Digite a hora inicial'}))
-	amoust = forms.IntegerField(min_value=1, widget=forms.NumberInput(
-                attrs={'size':'10'}))
 	limit = forms.TimeField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Digite o limite'}))
 	status = forms.BooleanField(widget=forms.CheckboxInput())
 	
 
 	class Meta:
 		model = Medicine
-		fields = ['name','interval','initial_time','amoust','limit', 'status','slot',]
+		fields = ['name','interval','initial_time','amount','limit', 'status','slot',]
 
 
 # Slot
